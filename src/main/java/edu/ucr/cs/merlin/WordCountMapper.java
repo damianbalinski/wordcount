11d82d7e83b8eb431e32da74f1ae5a756f6f744d
Damian Baliński <damian.balinski@infinite.pl>
2023-04-16 14:21:59 +0200

    Initial commit

---
+++ src/main/java/edu/ucr/cs/merlin/WordCountMapper.java
@@ -0,0 +1,23 @@
+package edu.ucr.cs.merlin;
+
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable>
+{
+	private Text wordToken = new Text();
+	public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException
+	{
+		StringTokenizer tokens = new StringTokenizer(value.toString()); //Dividing String into tokens
+		while (tokens.hasMoreTokens())
+		{
+			wordToken.set(tokens.nextToken());
+			context.write(wordToken, new IntWritable(1));
+		}
+	}
+}
