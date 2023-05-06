7f023390bb53b3a60cff3c6c3868aadb3731f060
Rafal Kiszczyszyn <rafalkiszczyszyn09@gmail.com>
2023-05-06 23:59:30 +0200

    Implement args parsing in LrcJob

--- src/main/java/com/app/mapreduce/lrc/LrcJob.java
+++ src/main/java/com/app/mapreduce/lrc/LrcJob.java
@@ -12,6 +12,8 @@ import org.apache.hadoop.util.GenericOptionsParser;
 import org.apache.log4j.PropertyConfigurator;
 
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 @Slf4j
 public class LrcJob {
@@ -24,9 +26,11 @@ public class LrcJob {
 
         Configuration conf = new Configuration();
         String[] pathArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
-        if (pathArgs.length < 2)
-        {
-            System.err.println("Usage: lrc-map-reduce <input-path> [...] <output-path>");
+        JobArgs jobArgs = new JobArgs();
+        try {
+            jobArgs.fromArgs(pathArgs);
+        } catch (IllegalArgumentException e) {
+            System.err.println(e.getMessage());
             System.exit(2);
         }
 
@@ -41,13 +45,56 @@ public class LrcJob {
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(DoubleWritable.class);
 
-        LrcReducer.N = 5;
+        LrcReducer.N = jobArgs.N;
 
-        for (int i = 0; i < pathArgs.length - 1; ++i)
+        for (String path : jobArgs.input)
         {
-            FileInputFormat.addInputPath(job, new Path(pathArgs[i]));
+            FileInputFormat.addInputPath(job, new Path(path));
         }
-        FileOutputFormat.setOutputPath(job, new Path(pathArgs[pathArgs.length - 1]));
+        FileOutputFormat.setOutputPath(job, new Path(jobArgs.output));
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static class JobArgs {
+        public int N = 25;
+        public String[] input;
+        public String output;
+
+        public void fromArgs(String[] args) {
+            Map<String, String> entries = parseArgs(args);
+
+            if (!entries.containsKey("input")) {
+                throw new IllegalArgumentException(
+                        "Missing input argument. --input=<comma separated list of input directories/files>.");
+            }
+
+            input = entries.get("input").split(",");
+
+            if (!entries.containsKey("output")) {
+                throw new IllegalArgumentException(
+                        "Missing output argument. --output=<output directory>.");
+            }
+
+            output = entries.get("output");
+
+            if (entries.containsKey("n")) {
+                N = Integer.parseInt(entries.get("n"));
+            }
+        }
+
+        private Map<String, String> parseArgs(String[] args) {
+            Map<String, String> map = new HashMap<>();
+            for (String arg: args) {
+                String[] tokens = arg.split("=", 2);
+                if (tokens.length != 2) {
+                    System.err.println("Invalid argument syntax: '" + arg + "'. Required syntax: --name=value.");
+                    System.exit(2);
+                }
+
+                map.put(tokens[0].substring(2), tokens[1]);
+            }
+
+            return map;
+        }
     }
 }
